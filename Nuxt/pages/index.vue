e511abdfd01e9fe9577e6b9e81187a6e9530bbb2
HasegawaTakatune <0ta5274738t4p4r@gmail.com>
2021-11-20 21:26:39 +0900

    Vueでの正規表現のテスト

--- Nuxt/pages/index.vue
+++ Nuxt/pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <Tutorial/>
+  <Tutorial />
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    var list = ["sample", "test"];
+    var test = new RegExp(list.join("|"), "g");
+    var str = ["min", "max", "testsadfasdf", "zxcvasdsample", "hoge"];
+
+    console.log(test);
+    for (var i = 0; i < str.length; i++) {
+      if (test.test(str[i])) {
+        console.log("マッチしたよ >> " + str[i]);
+      } else {
+        console.log("マッチしなかったよ");
+      }
+    }
+  },
+};
 </script>
